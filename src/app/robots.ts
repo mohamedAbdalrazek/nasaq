0295266b78275e53fd62cd56395268187a42463d
Mohamed Abdelrazek <mohamed.abdelrazek@schoolvoice.com>
2026-06-17 22:11:56 +0300

    Add example environment variables and implement robots.txt handling
    - Created a new example environment variables file (.dev.vars.example) with necessary keys for staging.
    - Added a new robots.ts file to manage robots.txt rules and sitemap URL dynamically based on the BASE_URL.
    - Removed the old static robots.txt file to streamline the configuration process.

---
+++ src/app/robots.ts
@@ -0,0 +1,13 @@
+import { BASE_URL } from "@/shared/lib/constants";
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+    return {
+        rules: {
+            userAgent: "*",
+            allow: "/",
+            disallow: "/confirmation",
+        },
+        sitemap: `${BASE_URL}/sitemap.xml`,
+    };
+}
